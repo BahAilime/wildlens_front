2f35677ec75b0c38c9e8a1047179800d6382c9c6
BahAilime <emilia.beguin@gmail.com>
2025-06-05 17:34:10 +0200

    fix with black

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 
 load_dotenv()
 
-BACKEND_BASE_URL = os.getenv('BACKEND_BASE_URL')
+BACKEND_BASE_URL = os.getenv("BACKEND_BASE_URL")
 print(BACKEND_BASE_URL)
 
 app = Flask(__name__)
@@ -13,21 +13,26 @@ app = Flask(__name__)
 csrf = CSRFProtect()
 csrf.init_app(app)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html', BACKEND_BASE_URL=BACKEND_BASE_URL)
+    return render_template("index.html", BACKEND_BASE_URL=BACKEND_BASE_URL)
 
-@app.route('/home')
+
+@app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
-@app.route('/scan')
+
+@app.route("/scan")
 def scan():
-    return render_template('scan.html')
+    return render_template("scan.html")
 
-@app.route('/base')
+
+@app.route("/base")
 def base():
-    return render_template('base.html')  # usually not directly rendered
+    return render_template("base.html")  # usually not directly rendered
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,17 +1,22 @@
 import pytest
 
+
 def test_index_route(client):
     """Vérifie que la route '/' renvoie une réponse 200 OK et le template correct."""
-    response = client.get('/')
+    response = client.get("/")
     assert response.status_code == 200
     assert b"Identifiez les empreintes de mammif" in response.data
 
+
 def test_home_route(client):
-    response = client.get('/home')
+    response = client.get("/home")
     assert response.status_code == 200
-    assert b"couvrez les animaux sauvages en scannant leurs empreintes." in response.data
+    assert (
+        b"couvrez les animaux sauvages en scannant leurs empreintes." in response.data
+    )
+
 
 def test_scan_route(client):
-    response = client.get('/scan')
+    response = client.get("/scan")
     assert response.status_code == 200
     assert b"Scannez une empreinte" in response.data
