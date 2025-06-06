3aa129f9ecaeacf0024a90bbd75a6757d14057af
BahAilime <emilia.beguin@gmail.com>
2025-06-06 14:59:28 +0200

    Ajout d'un exemple de donnée exif d'une image

---
+++ tests/unit/exif_exemple.js
@@ -0,0 +1,95 @@
+const exifExemple = {
+  "ImageDescription": "                               ",
+  "Make": "NIKON",
+  "Model": "COOLPIX P6000",
+  "Orientation": 1,
+  "XResolution": 300,
+  "YResolution": 300,
+  "ResolutionUnit": 2,
+  "Software": "Nikon Transfer 1.1 W",
+  "DateTime": "2008:11:01 21:15:07",
+  "YCbCrPositioning": 1,
+  "ExifIFDPointer": 268,
+  "GPSInfoIFDPointer": 926,
+  "ExposureTime": 0.013333333333333334,
+  "FNumber": 5.9,
+  "ExposureProgram": "Normal program",
+  "ISOSpeedRatings": 64,
+  "ExifVersion": "0220",
+  "DateTimeOriginal": "2008:10:22 16:28:39",
+  "DateTimeDigitized": "2008:10:22 16:28:39",
+  "ComponentsConfiguration": "YCbCr",
+  "ExposureBias": 0,
+  "MaxApertureValue": 2.9,
+  "MeteringMode": "Pattern",
+  "LightSource": "Unknown",
+  "Flash": "Flash did not fire, compulsory flash mode",
+  "FocalLength": 24,
+  "MakerNote": [
+    78,
+    0,
+    0,
+    0,
+    0
+  ],
+  "UserComment": [
+    65,
+    83,
+    32,
+    0
+  ],
+  "FlashpixVersion": "0100",
+  "ColorSpace": 1,
+  "PixelXDimension": 640,
+  "PixelYDimension": 480,
+  "InteroperabilityIFDPointer": 896,
+  "FileSource": "DSC",
+  "SceneType": "Directly photographed",
+  "CustomRendered": "Normal process",
+  "ExposureMode": 0,
+  "WhiteBalance": "Auto white balance",
+  "DigitalZoomRation": 0,
+  "FocalLengthIn35mmFilm": 112,
+  "SceneCaptureType": "Standard",
+  "GainControl": "None",
+  "Contrast": "Normal",
+  "Saturation": "Normal",
+  "Sharpness": "Normal",
+  "SubjectDistanceRange": "Unknown",
+  "GPSLatitudeRef": "N",
+  "GPSLatitude": [
+    43,
+    28,
+    2.814
+  ],
+  "GPSLongitudeRef": "E",
+  "GPSLongitude": [
+    11,
+    53,
+    6.45599999
+  ],
+  "GPSAltitudeRef": 0,
+  "GPSTimeStamp": [
+    14,
+    27,
+    7.24
+  ],
+  "GPSSatellites": "06",
+  "GPSImgDirectionRef": "\u0000",
+  "GPSMapDatum": "WGS-84   ",
+  "GPSDateStamp": "2008:10:23",
+  "thumbnail": {
+    "Compression": 6,
+    "XResolution": 72,
+    "YResolution": 72,
+    "ResolutionUnit": 2,
+    "JpegIFOffset": 4548,
+    "JpegIFByteCount": 6702,
+    "blob": {}
+  }
+}
+
+
+module.exports = {
+  exifExemple
+};
