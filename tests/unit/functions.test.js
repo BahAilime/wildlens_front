ff10e03f00ff04aff16b4795769eea612386e0ec
BahAilime <emilia.beguin@gmail.com>
2025-06-06 15:00:45 +0200

    Ajout de 4 tests de extractExifDate et empleexif

--- tests/unit/functions.test.js
+++ tests/unit/functions.test.js
@@ -1,23 +1,50 @@
-const { formatExifDate } = require("../../static/functions.js")
+const { formatExifDate, extractExifDate } = require("../../static/functions.js")
+const { exifExemple } = require("./exif_exemple.js");
 
-test("[HAPPY PATH] formatExifDate returns Date", () => {
-    expect(formatExifDate("2023:07:14 10:45:30")).toBeInstanceOf(Date);
+describe("formatExifDate", () => {
+
+    test("[HAPPY PATH] formatExifDate returns Date", () => {
+        expect(formatExifDate("2023:07:14 10:45:30")).toBeInstanceOf(Date);
+    });
+
+    test("[HAPPY PATH] formatExifDate returns the correct date", () => {
+        expect(formatExifDate("2023:07:14 10:45:30")).toBeInstanceOf(Date);
+        expect(formatExifDate("2023:07:14 10:45:30")).toEqual(new Date("2023-07-14T10:45:30"));
+    });
+
+    test("[HAPPY PATH] formatExifDate returns the correct date when the input is a number", () => {
+        expect(formatExifDate(628021800000)).toBeInstanceOf(Date);
+        expect(formatExifDate(628021800000)).toEqual(new Date("1989-11-25T18:30:00.000Z"));
+    });
+
+    test("[BAD PATH] formatExifDate with no argument throws type error", () => {
+        expect(() => formatExifDate()).toThrowError(TypeError);
+    });
+
+    test("[BAD PATH] formatExifDate with an array argument throws type error", () => {
+        expect(() => formatExifDate(["test"])).toThrowError(TypeError);
+    });
+
 });
 
-test("[HAPPY PATH] formatExifDate returns the correct date", () => {
-    expect(formatExifDate("2023:07:14 10:45:30")).toBeInstanceOf(Date);
-    expect(formatExifDate("2023:07:14 10:45:30")).toEqual(new Date("2023-07-14T10:45:30"));
-});
+describe("exifExemple", () => {
+    test("exifExemple returns an object", () => {
+        expect(exifExemple).toBeInstanceOf(Object);
+    });
+})
 
-test("[HAPPY PATH] formatExifDate returns the correct date when the input is a number", () => {
-    expect(formatExifDate(628021800000)).toBeInstanceOf(Date);
-    expect(formatExifDate(628021800000)).toEqual(new Date("1989-11-25T18:30:00.000Z"));
-});
+describe("extractExifDate", () => {
+    test("[HAPPY PATH] extractExifDate returns a date", () => {
+        expect(extractExifDate(exifExemple)).toBeInstanceOf(Date);
+    });
 
-test("[BAD PATH] formatExifDate with no argument throws type error", () => {
-    expect(() =>formatExifDate()).toThrowError(TypeError);
-});
+    test("[HAPPY PATH] extractExifDate returns current date if no date is found", () => {
+        expect(extractExifDate({})).toBeInstanceOf(Date);
+        expect(extractExifDate({})).toEqual(new Date());
+    });
 
-test("[BAD PATH] formatExifDate with an array argument throws type error", () => {
-    expect(() =>formatExifDate(["test"])).toThrowError(TypeError);
-});
+    test("[HAPPY PATH] extractExifDate returns ", () => {
+        expect(() => extractExifDate()).toThrowError(TypeError);
+    });
+})
+
